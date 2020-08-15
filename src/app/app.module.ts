0300f9e9b82f0b112942c6d4366e59ef2ebe5a6b
zahra <zahrajoulaei@gmail.com>
2020-08-15 23:21:28 +0200

    :sparkles: a button for adding usernames,use 2-way binding and string interpolation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { SuccessAlertComponent } from './success-alert/success-alert.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,13 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerComponent implements OnInit {
   allowNewServe = false;
+  serverCreatinStatus = "No server is created!";
   Serverid: number = 120;
-  getServerid :string = 'sahih';
+  getServerid :string = 'Running perfect';
+  serverName ='';
+  userName='';
+  userNameStatus ="username is not created";
   constructor() {
     setTimeout(() => {this.allowNewServe = true; }, 2000);
   }
 
   ngOnInit() {
   }
+  OnserverCreatin() {
+    this.serverCreatinStatus = "Server just created! its name is :" + this.serverName;
+  }
+  onusernameClick() {
+     this.userNameStatus = "username is: " + this.userName;
+  }
+  OnUpdateServerName(event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
 
+  }
 }
