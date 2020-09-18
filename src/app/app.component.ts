91e87cd68eb1c3ddd3a27ece12233fbc15b6fff4
zahra <zahrajoulaei@gmail.com>
2020-09-18 18:55:20 +0430

    getting the index

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent {
 
     toggleSecret(){
       this.showSecret = !this.showSecret;
-      this.log.push(this.log.length +1);
+      this.log.push(new Date());
     }
 
 }
