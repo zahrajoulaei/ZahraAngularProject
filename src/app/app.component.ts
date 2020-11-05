9b3ffc429b51ec3781c6f623c393fb00d2467887
zahra <zahrajoulaei@gmail.com>
2020-11-05 18:04:42 +0330

    event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,15 @@ export class AppComponent {
   title = 'udemy-project';
   showSecret = false;
     log= [];
-
+    loadedFeature = 'recipe';
 
     toggleSecret(){
       this.showSecret = !this.showSecret;
       this.log.push(new Date());
     }
+
+  onNavigate(feature: string) {
+  this.loadedFeature =feature;
+  }
 
 }
